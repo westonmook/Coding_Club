c82590836e010959b3968afe3caee0bb62f69fc0
westonmook <westonmook@gmail.com>
2025-09-05 10:20:35 -0600

    Create p5a2.py

---
+++ WS/WS1/p5a2.py
@@ -0,0 +1,6 @@
+first_name = "John"     # Replace with your first name
+last_name = "Doe"       # Replace with your last name
+
+full_name = first_name + " " + last_name
+
+print("Full name:", full_name)
