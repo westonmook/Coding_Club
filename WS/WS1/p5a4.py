dfd554057a8094053b391157d6434dccf8290d78
westonmook <westonmook@gmail.com>
2025-09-05 10:23:49 -0600

    Create p5a4.py

---
+++ WS/WS1/p5a4.py
@@ -0,0 +1,9 @@
+
+word = "Python"
+print(word[:3])
+#prints the second character y
+print(word[1])
+#prints the first character P
+print(word[0])
+#prints the characters y through h
+print(word[1:4])
