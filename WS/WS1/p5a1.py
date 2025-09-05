f1e00569490e6dd93f518311af35fe1b76673a29
westonmook <westonmook@gmail.com>
2025-09-05 10:19:08 -0600

    Create p5a1.py

---
+++ WS/WS1/p5a1.py
@@ -0,0 +1,8 @@
+# Create a string variable 'greeting'
+greeting = "Hello, world!"
+
+# Print the length of the string
+print("Length of greeting:", len(greeting))
+
+# Print the first character of the string
+print("First character of greeting:", greeting[0])
