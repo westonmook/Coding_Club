317690ddb4bf08b2734c201cec2edd8ae7d0b793
westonmook <westonmook@gmail.com>
2025-09-05 12:03:20 -0600

    Create b1.py

---
+++ BreakFix/b1.py
@@ -0,0 +1,11 @@
+#fix this without removing anything.
+x
+print(brokenstring)
+print( x + 2)
+y = x + 1
+if y + x == 7
+       print('y is true')
+if True == False:
+print('The world might be broken')
+for i range(len(x)):
+    print('You did it!')
