48a53139344c2dda366b8ae9f61b74fcaba1c922
westonmook <westonmook@gmail.com>
2025-09-26 11:32:05 -0600

    Create p5a1-2.py

---
+++ WS/WS2/p5a1-2.py
@@ -0,0 +1,12 @@
+#p5a1
+stack = []
+#p5a2
+stack.append('first in')
+stack.append('last out')
+print(stack)
+#pop() removes and returns the last item in a list. 
+#This is usually the last item added to a list
+print(stack.pop())
+print(stack)
+print(stack.pop())
+print(stack)
