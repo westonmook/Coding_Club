458267ae665a1c46f22559f89cddcd0e3fa1999f
westonmook <westonmook@gmail.com>
2025-10-24 10:05:15 -0600

    minor game update

--- PyGame/TestGame1.py
+++ PyGame/TestGame1.py
@@ -1,11 +1,20 @@
 import pygame as pg
 import random
+
+
 #always initialize
 pg.init()
 #menu
 screen = pg.display.set_mode((800, 600))
 #background image
 background = pg.image.load('resources/bg1.jpg')
+
+#sound
+###mixer.music.load('filename.wav')
+##-1 is a musical loop
+## var = mixer.Sound('filename.wav') for calling smaller sounds
+## var.play()
+###mixer.music.play(-1)
 
 #title bar
 pg.display.set_caption("Space Invaders")
@@ -28,6 +37,7 @@ alienY = []
 alienX_change = []
 alienY_change = []
 q_of_aliens = 7
+#alien_t_switch = 0
 #old variables
 for i in range(q_of_aliens):
     alienImg.append(pg.image.load('resources/alien.png'))
@@ -56,15 +66,34 @@ def cannon_alt():
 laserY_change = 0.5
 laser_state = "ready"
 
+#alien explosion
+explosionImg = pg.image.load('resources/explode.png')
+
 #Scoring
 score = 0
+##font always .ttf
+font = pg.font.Font('freesansbold.ttf', 36)
+textX = 10
+textY = 10
+def show_score(x, y):
+    scorer = font.render("Aliens Obliterated : " + str(score), True, (75, 205, 95))
+    screen.blit(scorer, (x, y))
+
+#Game Over
+GOfont = pg.font.Font('freesansbold.ttf', 80)
+def game_over(x,y):
+    gotext = font.render("Aliens Obliterated... YOU!", True, (245, 205, 95))
+    screen.blit(gotext, (250,300))
 
 #Alien function
 def alien(x, y, i):
     screen.blit(alienImg[i], (x, y))
 
 def alien_respawn(i):
-    global alienX, alienY
+    global alienX, alienY, alien_t_switch
+    #alien_t_switch += 1
+    #if alien_t_switch == 3:
+        #alienImg[i] = pg.image.load('resources/alien2.png')
     alienX[i] = random.randint(0, 800)
     alienY[i] = random.randint(20, 200)
 
@@ -122,21 +151,29 @@ while running:
 
     #alien boundary
     for i in range(q_of_aliens):
+
+        #Game Over
+        if alienY[i] > 440:
+            for j in range(q_of_aliens):
+                alienY[j] = 2000
+            game_over()
+            break
+
         alienX[i] += alienX_change[i]
         if alienX[i] <= -20:
             alienX_change[i] = 0.2
             alienY[i] +=25
         elif alienX[i] >= 756:
             alienX_change[i] = -0.2
-            alienY[i] += 25
-            
+            alienY[i] += 35
+
         #collision
         collision = isCollision(alienX[i], alienY[i], laserX, laserY, i)
         if collision:
             laserY = 480
             laser_state = "ready"
             score += 1
-            print("your score is: " + str(score))
+            #print("your score is: " + str(score))
             alien_respawn(i)
         alien(alienX[i], alienY[i], i)
 
@@ -149,7 +186,8 @@ while running:
         fire_laser(laserX, laserY)
         if laserY < -60:
             laser_state = "ready"
-    
+    #score
+    show_score(textX, textY)
 
     pg.display.update()
     
